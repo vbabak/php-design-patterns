c04cc5438601711c2cb246267b05900e98838d8a
Vladyslav Babak <marmalade.vlad@gmail.com>
2017-11-04 15:53:51 +0200

    Factory Method

---
+++ README.md
@@ -0,0 +1,10 @@
+PHP Design Patterns
+========================
+
+Most frequently used design patterns in PHP language. 
+> Requires PHP >= 7.1
+
+## Creational
+[Factory Method](creational/FactoryMethod/FactoryMethod.md)
+
+
---
+++ creational/FactoryMethod/FactoryMethod.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types = 1);
+
+interface Product
+{
+    public function getCalories(): float;
+}
+
+class Apple implements Product
+{
+    public function getCalories(): float
+    {
+        $calories = 52 / 100;
+
+        return $calories;
+    }
+}
+
+class Orange implements Product
+{
+    public function getCalories(): float
+    {
+        $calories = 47 / 100;
+
+        return $calories;
+    }
+}
+
+class ProductFactory
+{
+    public function create(string $type)
+    {
+        $instance = null;
+        if ($type === 'orange') {
+            $instance = new Orange();
+        } else if ($type === 'apple') {
+            $instance = new Apple();
+        }
+
+        return $instance;
+    }
+}
+
+class CaloriesCalculator
+{
+    public function calculateCalories($weight, $type): float
+    {
+        $productFactory = new ProductFactory();
+        $product = $productFactory->create($type);
+        if (!$product instanceof Product) {
+            throw new Exception('Unknown product type');
+        }
+        $calories = $product->getCalories();
+        $total = $calories * $weight;
+
+        return $total;
+    }
+}
+
+$calories_calculator = new CaloriesCalculator();
+
+$p_type = 'apple';
+$p_weight = 100;
+
+$total_calories = $calories_calculator->calculateCalories($p_weight, $p_type);
+echo sprintf('%dg. of %s has %.0F calories', $p_weight, $p_type, $total_calories);
