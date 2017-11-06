16ab3b1577ad8cb4bbb3b93bb099235107a15b36
Vladyslav Babak <marmalade.vlad@gmail.com>
2017-11-06 08:26:24 +0200

    Abstract Factory

---
+++ creational/AbstractFactory/AbstractFactory.php
@@ -0,0 +1,112 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Application;
+
+interface PizzaInterface
+{
+    public function prepare($choice): void;
+}
+
+interface SpiceInterface
+{
+}
+
+
+interface CheeseInterface
+{
+}
+
+
+class RedPepper implements SpiceInterface
+{
+}
+
+
+class Salt implements SpiceInterface
+{
+}
+
+
+class GoodCheese implements CheeseInterface
+{
+}
+
+
+class SpicyCheese implements CheeseInterface
+{
+}
+
+abstract class PizzaComponentFactoryAbstract
+{
+    public abstract function createSpice(): SpiceInterface;
+
+    public abstract function createCheese(): CheeseInterface;
+}
+
+class LitePizzaComponentFactory extends PizzaComponentFactoryAbstract
+{
+    public function createSpice(): SpiceInterface
+    {
+        return new Salt();
+    }
+
+    public function createCheese(): CheeseInterface
+    {
+        return new GoodCheese();
+    }
+}
+
+class SpicyPizzaComponentFactory extends PizzaComponentFactoryAbstract
+{
+    public function createSpice(): SpiceInterface
+    {
+        return new RedPepper();
+    }
+
+    public function createCheese(): CheeseInterface
+    {
+        return new SpicyCheese();
+    }
+}
+
+class PizzaComponentFactoryProducer
+{
+    public function getComponentFactory($choice): PizzaComponentFactoryAbstract
+    {
+        if ($choice === 'lite') {
+            return new LitePizzaComponentFactory();
+        } else if ($choice === 'spicy') {
+            return new SpicyPizzaComponentFactory();
+        } else {
+            throw new \Exception('Unknown $choise');
+        }
+    }
+}
+
+class Pizza implements PizzaInterface
+{
+    public function prepare($choice): void
+    {
+        $componentsFactoryProducer = new PizzaComponentFactoryProducer();
+        $componentsFactory = $componentsFactoryProducer->getComponentFactory($choice);
+        $spice = $componentsFactory->createSpice();
+        $this->addSpice($spice);
+        $cheese = $componentsFactory->createCheese();
+        $this->addCheese($cheese);
+    }
+
+    protected function addSpice($spice)
+    {
+        echo 'Added spice' . PHP_EOL;
+    }
+
+    protected function addCheese($cheese)
+    {
+        echo 'Added cheese' . PHP_EOL;
+    }
+}
+
+$pizza = new Pizza();
+$pizza->prepare('spicy');
---
+++ creational/AbstractFactory/README.md
@@ -0,0 +1,14 @@
+Abstract Factory
+========================
+
+## Intent
+
+Provide an interface for creating families of related or 
+dependent objects without specifying their concrete classes.
+
+## Implementation
+
+
+![alt Abstract Factory Method UML Diagram](AbstractFactory.png)
+
+[PHP Implementation of Abstract Factory](AbstractFactory.php)
